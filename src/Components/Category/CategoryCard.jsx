308a2484d8741c01e885185c46b3c6fed9078365
BrianLigh <brianlight2002@yahoo.com>
2024-05-28 01:37:53 -0700

    Category + Category Card + CategoryFullInfo

--- src/Components/Category/CategoryCard.jsx
+++ src/Components/Category/CategoryCard.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import classes from './category.module.css'
-import { Link } from 'react-router-dom'
+import classes from "./category.module.css";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function CategoryCard({data}) {
+function CategoryCard({ data }) {
   return (
     <div className={classes.category}>
       <Link to={`/category/${data.name}`}>
         <span>
-            <h2>{data?.title}</h2>
+          <h2>{data?.title}</h2>
         </span>
         <img src={data?.imgLink} alt="" />
         <p>Shop now</p>
       </Link>
     </div>
-  )
+  );
 }
 
-export default CategoryCard
+// Defining the prop types
+CategoryCard.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    imgLink: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default CategoryCard;
