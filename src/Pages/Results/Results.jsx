02bf303ee4067071b97f67cb4684f6ac2faae179
BrianLigh <brianlight2002@yahoo.com>
2024-05-28 01:39:44 -0700

    Cart and Results

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import LayOut from "../../Components/LayOut/LayOut";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -39,7 +39,7 @@ function Results() {
             <div className={classes.products__container}>
               {results?.map((product) => (
                 <ProductCard
-                  Key={product.id}
+                  key={product.id}
                   product={product}
                   renderDesc={false}
                   renderAdd={true}
