8b53d03c0d2abcce4e412c3817e1dc96fd4ddfcc
BrianLigh <brianlight2002@yahoo.com>
2024-05-28 01:39:28 -0700

    Product + Product Card and Product Detail

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -1,39 +1,46 @@
-import React, { useContext } from 'react'
-import Rating from '@mui/material/Rating'
-import CurrencyFormat from '../CurrencyFormat/CurrencyFormat'
-import classes from './Product.module.css'
-import { Link } from 'react-router-dom';
-import {DataContext} from '../DataProvider/DataProvider'
-import { Type } from '../../Utility/action.type';
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import Rating from "@mui/material/Rating";
+import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
+import classes from "./Product.module.css";
+import { Link } from "react-router-dom";
+import { DataContext } from "../DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type";
 
-function ProductCard({product, flex, renderDesc, renderAdd}) {
-  const {image, title, id, rating, price, description} = product;
-  
-  const [state,dispatch] =useContext(DataContext)
-  
-  
+function ProductCard({ product, flex, renderDesc, renderAdd }) {
+  const { id, image, title, rating, price, description } = product;
 
-  const addToCart = ()=>{
+  const [, dispatch] = useContext(DataContext);
+
+  const addToCart = () => {
     dispatch({
-      type:Type.ADD_TO_BASKET,
-      item:{
-        image, title, id, rating, price, description
-      }
-    })
-  }
-
+      type: Type.ADD_TO_BASKET,
+      item: {
+        image,
+        title,
+        id,
+        rating,
+        price,
+        description,
+      },
+    });
+  };
 
   return (
-    <div className={`${classes.card__container} ${flex?classes.product__flexed :''}`}>
+    <div
+      className={`${classes.card__container} ${
+        flex ? classes.product__flexed : ""
+      }`}
+    >
       <Link to={`/products/${id}`}>
         <img src={image} alt="" />
       </Link>
       <div>
         <h3>{title}</h3>
-       { renderDesc && <div style={{maxWidth : "800px"}}>{description}</div>}
+        {renderDesc && <div style={{ maxWidth: "800px" }}>{description}</div>}
         <div className={classes.rating}>
-          {/* { * rating */} 
-        <Rating value= {rating?.rate} precision={0.1}/>
+          {/* { * rating */}
+          <Rating value={rating?.rate} precision={0.1} />
           {/* racing counter */}
           <small> {rating?.count} </small>
         </div>
@@ -42,16 +49,32 @@ function ProductCard({product, flex, renderDesc, renderAdd}) {
           <CurrencyFormat amount={price} />
         </div>
 
-        {
-          renderAdd && <button className={classes.button} onClick={addToCart} >
-          add to cart
-        </button>
-        }
-        
-
+        {renderAdd && (
+          <button className={classes.button} onClick={addToCart}>
+            add to cart
+          </button>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default ProductCard
+// Define the prop type
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    rating: PropTypes.shape({
+      rate: PropTypes.number,
+      count: PropTypes.number,
+    }),
+    price: PropTypes.number.isRequired,
+    description: PropTypes.string,
+  }).isRequired,
+  flex: PropTypes.bool,
+  renderDesc: PropTypes.bool,
+  renderAdd: PropTypes.bool,
+};
+
+export default ProductCard;
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import LayOut from "../../Components/LayOut/LayOut";
-import classes from "./ProductDetail.module.css";
+// import classes from "./ProductDetail.module.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { productUrl } from "../../Api/endPoints";
@@ -8,36 +8,37 @@ import ProductCard from "../../Components/Product/ProductCard";
 import Loader from "../../Components/Loader/Loader";
 
 function ProductDetail() {
-
   const [product, setproduct] = useState({});
-  const[isLoading, setisLoading] = useState(false)
+  const [isLoading, setisLoading] = useState(false);
   const { productId } = useParams();
-  
+
   useEffect(() => {
-    setisLoading(true)
+    setisLoading(true);
     axios
       .get(`${productUrl}/products/${productId}`)
       .then((res) => {
         setproduct(res.data);
-        setisLoading(false)
+        setisLoading(false);
         // console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
-        setisLoading(false)
-      })
+        setisLoading(false);
+      });
   }, []);
 
   return (
     <LayOut>
-      {isLoading? (<Loader/>):( <ProductCard 
-      
-      product={product} 
-      flex ={true}
-      renderDesc ={true}
-      renderAdd={true}
-      />)}
-      
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ProductCard
+          product={product}
+          flex={true}
+          renderDesc={true}
+          renderAdd={true}
+        />
+      )}
     </LayOut>
   );
 }
