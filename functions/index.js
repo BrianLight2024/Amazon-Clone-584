fca3efb3bb383813d4b4170313220d26364ea9a9
BrianLigh <brianlight2002@yahoo.com>
2024-05-29 05:20:51 -0700

    Firebase Cloud Deployment Setup

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,19 @@
 const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { setGlobalOptions } = require("firebase-functions/v2");
+const Stripe = require("stripe");
+
+// Load environment variables from .env file
 dotenv.config();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const stripe = Stripe(process.env.STRIPE_KEY);
 
 const app = express();
-app.use(cors({ origin: true }));
 
+setGlobalOptions({ maxInstances: 10 });
+app.use(cors({ origin: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -20,16 +25,20 @@ app.get("/", (req, res) => {
 app.post("/payment/create", async (req, res) => {
   const total = parseInt(req.query.total);
   if (total > 0) {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: total,
-      currency: "usd",
-    });
-    res.status(201).json({
-      clientSecret: paymentIntent.client_secret,
-    });
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+      });
+      res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   } else {
     res.status(403).json({
-      message: "total must be greater than 0.",
+      message: "Total must be greater than 0.",
     });
   }
 });
